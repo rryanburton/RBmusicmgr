d2546f03ed268b17cd89039843e3a32219b9fc8a
Ryan Burton <rryanburton@gmail.com>
2015-12-15 21:44:30 -0500

    django rest framework and models installed

--- RBmusicmgr/settings.py
+++ RBmusicmgr/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mgrApp',
+    'rest_framework',
+    'rest_framework_swagger',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'RBmusicmgr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rbmgr-db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -102,3 +108,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- mgrApp/admin.py
+++ mgrApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Client, Musician
 # Register your models here.
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'artist_name']
+
+
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'band', 'instrument']
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Musician, MusicianAdmin)
